91d8916140255624e09ac1301530d14e8e4553b1
Alexander "Ace" Olofsson <ace@haxalot.com>
2019-08-29 17:35:24 +0200

    Add in missing parts to main lib file

--- lib/git_work_tracker.rb
+++ lib/git_work_tracker.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
 require 'git_work_tracker/version'
+require 'git'
+require 'logging'
 
 module GitWorkTracker
+  autoload :Repository, 'git_work_tracker/repository'
+
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.debug!
+    logger.level = :debug
+  end
+
+  def self.logger
+    @logger ||= ::Logging.logger[self].tap do |logger|
+      logger.add_appenders ::Logging.appenders.stdout
+      logger.level = :info
+    end
+  end
 end
+
+GitWorkTracker.logger # TODO: do more appropriately
